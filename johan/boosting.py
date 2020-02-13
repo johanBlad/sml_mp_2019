8d7934cea3b3149d70942a071f68f09e0ebbb05b
jblad <johan.blad@hotmail.se>
2020-02-13 09:09:18 +0100

    Updated code to cross-validate on model NOT trained on all data

--- johan/boosting.py
+++ johan/boosting.py
@@ -5,6 +5,7 @@ import sklearn.tree as sk_tree
 from sklearn.ensemble import AdaBoostClassifier
 import numpy as np
 import os
+import sklearn.model_selection as skl_ms
 
 data_dict = initialize_data()
 
@@ -12,30 +13,39 @@ x_train = data_dict.get('x_train')
 y_train = data_dict.get('y_train')
 x_test = data_dict.get('x_test')
 
-depth = 1
-lr = 0.035
-n_est = 350
+depth = 2
+lr = 0.017
+n_est = 150
+
+n_fold = 10
+
+accuracy = np.zeros(n_fold)
 
 base_learner = sk_tree.DecisionTreeClassifier(
     criterion='entropy', max_depth=depth)
-AdaBoost = AdaBoostClassifier(
+model = AdaBoostClassifier(
     base_estimator=base_learner, n_estimators=n_est, learning_rate=lr)
-model = AdaBoost.fit(x_train, y_train)
 
-val_stats = mod_sel.cross_validate(model, x_train, y=y_train, cv=10)
-avg_score = np.mean(val_stats.get('test_score'))
+cv = skl_ms.KFold(n_fold, shuffle=True, random_state=1)
 
-print('\nTest scores:', val_stats.get('test_score'))
-print('Average: ', avg_score, '\n')
+for i, (train_index, val_index) in enumerate(cv.split(x_train)):
+    x_train_l, x_val = x_train[train_index], x_train[val_index]
+    y_train_l, y_val = y_train[train_index], y_train[val_index]
+
+    model.fit(x_train_l, y_train_l)
+    prediciton = model.predict(x_val)
+    accuracy[i] = 1 - np.mean(prediciton != y_val)
+
+avg_acc = np.mean(accuracy)
+print('Accuracy:', accuracy)
+print('Average: ', avg_acc, '\n')
 
 prediction = predict(model, x_test)
 
-if True:
+if False:
     path = os.getcwd() + "/src/python/sml/mp/"
     txt_file = open(
-        f'{path}manual_saved/{avg_score:2f} | AdaBoost | lr={lr:2f} | d={depth} | n_est={n_est}.txt', 'w')
+        f'{path}manual_saved/{avg_acc:2f} | AdaBoost | lr={lr:2f} | d={depth} | n_est={n_est}.txt', 'w')
     txt_file.write(''.join(str(x) for x in prediction))
     txt_file.close()
-    print(f'Write to file with acc: {avg_score:2f}, d={depth}')
-
-print(prediction)
+    print(f'Write to file with acc: {avg_acc:2f}, d={depth}')
--- johan/boosting_loop.py
+++ johan/boosting_loop.py
@@ -18,25 +18,39 @@ x_test = data_dict.get('x_test')
 
 
 depths = [1, 2]
-estimater_choices = range(100, 700, 50)
+estimater_choices = range(250, 500, 25)
 
 while True:
-    lr = rnd.random()/rnd.randint(1,10)
+    lr = rnd.random()/rnd.randint(3,7)
     depth = rnd.choice(depths)
     n_est = rnd.choice(estimater_choices)
 
-    base_learner = sk_tree.DecisionTreeClassifier(criterion='entropy', max_depth=depth)
-    AdaBoost = AdaBoostClassifier(base_estimator=base_learner, n_estimators=n_est, learning_rate=lr)
-    model = AdaBoost.fit(x_train, y_train)
-    val_stats = mod_sel.cross_validate(model, x_train, y=y_train, cv=10)
+    n_fold = 10
+    accuracy = np.zeros(n_fold)
+    cv = mod_sel.KFold(n_fold, shuffle=True, random_state=1)
 
-    avg_score = np.mean(val_stats.get('test_score'))
+    base_learner = sk_tree.DecisionTreeClassifier(
+        criterion='entropy', max_depth=depth)
+    model = AdaBoostClassifier(
+        base_estimator=base_learner, n_estimators=n_est, learning_rate=lr)
+
+    for i, (train_index, val_index) in enumerate(cv.split(x_train)):
+        x_train_l, x_val = x_train[train_index], x_train[val_index]
+        y_train_l, y_val = y_train[train_index], y_train[val_index]
+
+        model.fit(x_train_l, y_train_l)
+        prediciton = model.predict(x_val)
+        accuracy[i] = 1 - np.mean(prediciton != y_val)
+    avg_acc = np.mean(accuracy)
+
+    model = model.fit(x_train, y_train)
     prediction = predict(model, x_test)
-    if ((avg_score > 0.82 and depth == 1) or (avg_score > 0.83 and depth == 2)):
-        txt_file = open(f'{path}saved_models/{avg_score:2f} | AdaBoost | lr={lr:2f} | d={depth} | n_est={n_est}.txt', 'w')
+
+    if ((avg_acc > 0.824 and depth == 1) or (avg_acc > 0.835 and depth > 1)):
+        txt_file = open(f'{path}saved_models2/{avg_acc:2f} | AdaBoost | lr={lr:2f} | d={depth} | n_est={n_est}.txt', 'w')
         txt_file.write(''.join(str(x) for x in prediction))
         txt_file.close()
-        print(f'Write to file with acc: {avg_score:2f}, d={depth}')
+        print(f'Write to file with acc: {avg_acc:2f}, d={depth}')
 
 print("DONE")
 
